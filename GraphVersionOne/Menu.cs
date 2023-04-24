5036be4b4e5b7054a26aec5311549aac9f1cc38b
Ifeoma Okafor <if.comokafor@ymail.com>
2023-04-24 15:52:34 +0100

    untracked changes to menu-class saved

--- GraphVersionOne/Menu.cs
+++ GraphVersionOne/Menu.cs
@@ -5,74 +5,13 @@ namespace ConsoleApp3
 {
 	public class Menu
 	{
-		private GraphController controller;
+		private MenuController controller;
 
 		public Menu()
 		{
-			controller = new GraphController();
+			controller = new MenuController();
 		}
 
-        public void managerMenu()
-        {
-            Console.WriteLine("Please enter the secret password to access this menu");
-            int password = Convert.ToInt32(Console.ReadLine());
-
-            switch (password)
-            {
-
-            }
-
-        }
-
-        public void customerMenu()
-        {
-            Console.WriteLine("##################################");
-            Console.WriteLine("#      Transport for London      #");
-            Console.WriteLine("#    Getting you there faster    #");
-            Console.WriteLine("################################## ");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("1. Customer Menu ");
-            Console.WriteLine("2. Admin Menu ");
-            Console.WriteLine();
-            Console.WriteLine("   Please enter your choice: ");
-
-            int customerOption = Convert.ToInt32(Console.ReadLine());
-
-            switch (customerOption)
-            {
-                case 1:
-                    {
-
-                        Console.WriteLine("What is the Station you would like to start from?");
-                        string sourceStation = Convert.ToString(Console.ReadLine()) ?? "";
-
-                        if (!sourceStation.GetType().Equals(typeof(string)))
-                        {
-                            throw new ArgumentException("Input string must be a string data type.", "sourceStation");
-                        }
-
-                        Console.WriteLine("What is the Station you would like to start from?");
-                        string destStation = Convert.ToString(Console.ReadLine()) ?? "";
-
-                        if (!destStation.GetType().Equals(typeof(string)))
-                        {
-                            throw new ArgumentException("Input string must be a string data type.", "destStation");
-                        }
-
-                        controller.FindFastestWalkingRoutes(sourceStation,destStation);
-                    }
-                    break;
-
-                case 2:
-                    {
-
-                    }
-                    break;
-            }
-
-
-        }
 
         public void runMenu()
         {
@@ -95,14 +34,14 @@ namespace ConsoleApp3
             {
                 case 1:
                     {
-                       //access customer menu
+                        controller.customerMenu();
                     }
 
                     break;
 
                 case 2:
                     {
-                        //access manager menu
+                        controller.managerMenu();
                     }
 
                     break;
@@ -115,19 +54,7 @@ namespace ConsoleApp3
             }
         }
 
-        public void DoSomething(string str)
-        {
-            if (str == null)
-            {
-                throw new ArgumentNullException("str", "Input string cannot be null.");
-            }
-            else if (!str.GetType().Equals(typeof(string)))
-            {
-                throw new ArgumentException("Input string must be a string data type.", "str");
-            }
-
-            // rest of your code here...
-        }
+      
 
     }
 }
--- GraphVersionOne/PriorityQueueNode.cs
+++ GraphVersionOne/PriorityQueueNode.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-namespace GraphVersionOne
+namespace ConsoleApp3
 {
-	public class PriorityQueueNode
-	{
-		public PriorityQueueNode()
-		{
-		}
-	}
+    public class PriorityQueueNode
+    {
+        public Station station { get; set; }
+        public int distance { get; set; }
+
+        public PriorityQueueNode(Station station, int distance)
+        {
+            this.station = station;
+            this.distance = distance;
+        }
+    }
 }
 
