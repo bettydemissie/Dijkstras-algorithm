fcf5d96d53ee53850efa6209b32617b4d5ed97a7
Ifeoma Okafor <if.comokafor@ymail.com>
2023-04-24 13:13:22 +0100

    modified station class to closely resemble information on TfL website

--- GraphVersionOne/Station.cs
+++ GraphVersionOne/Station.cs
@@ -1,23 +1,71 @@
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
 namespace ConsoleApp3;
 
-public class Station
+public class Station : AccessStatus
 {
-    private string name;
+    private static int SID = 0;
+    private int StationID;                       // a unique ID number 
+    private string name;                          // name                      
+    private string TubeLine;                     // TubeLine
+    private AccessStatus.ACCESS StationAccess;   // access method
+    private int TravelZone;                      // Travel Zone its on
+    private AccessStatus.STATUS StationStatus;   // open or closed
 
+
+    public Station(string name, string TubeLine,
+                AccessStatus.ACCESS StationAccess, int TravelZone,
+                AccessStatus.STATUS StationStatus)
+    {
+        this.StationID = SID++;
+        this.name = name;
+        this.TubeLine = TubeLine;
+        this.StationAccess = StationAccess;
+        this.TravelZone = TravelZone;
+        this.StationStatus = StationStatus;
+
+    }
+
+
+    //initialised constructor for the current tests, will delete when other parameters
+    //in this constructor is initialised in Station implementation
     public Station(string name)
     {
+        this.StationID = SID++;
         this.name = name;
-      
+        this.TubeLine = TubeLine;
+        this.StationAccess = StationAccess;
+        this.TravelZone = TravelZone;
+        this.StationStatus = StationStatus;
+
     }
-    
-    public override string ToString()
+
+    public void setStationID(int StationID)
     {
-        return
-            $"{name}";
+        this.StationID = StationID;
+    }
+
+    public int getStationID()
+    {
+        return StationID;
     }
 
     public string getName()
     {
         return name;
     }
+
+    public override string ToString()
+    {
+        return
+            $"{name}" +
+            $"{StationID.ToString()}" +
+            $"{TubeLine}" +
+            $"{StationAccess.ToString()}" +
+            $"{TravelZone.ToString()}" +
+            $"{StationStatus.ToString()}";
+    }
+
+    
 }
