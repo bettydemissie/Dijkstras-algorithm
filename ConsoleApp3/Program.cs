fa00f1738c3d479dc63b62da6f89dcb983fa25fc
Ifeoma Okafor <if.comokafor@ymail.com>
2023-05-01 20:45:21 +0100

    changes to the stationcreation class

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,21 +3,21 @@ using ConsoleApp3;
 
 Menu newMenu = new Menu();
 
-string filepath = "/Users/Ifeoma1/Downloads/Zone-1-walkingdistance.xlsx";
-string filepathstations = "/Users/Ifeoma1/Downloads/StationsExcel.xlsx";
+//string filepath = "/Users/Ifeoma1/Downloads/Zone-1-walkingdistance.xlsx";
+//string filepathstations = "/Users/Ifeoma1/Downloads/StationsExcel.xlsx";
 
-ReadExcel read = new ReadExcel();
-ReadStationsandNetworks text = new ReadStationsandNetworks(read);
-var stations = text.GetTheStations(filepathstations);
+//ReadExcel read = new ReadExcel();
+//ReadStationsandNetworks text = new ReadStationsandNetworks(read);
+//var stations = text.GetTheStations(filepathstations);
 
-var networks = text.GetNetworks(filepath);
+//var networks = text.GetNetworks(filepath);
 
-foreach (Network item in networks)
-{
-    Console.WriteLine(item);
+//foreach (Network item in networks)
+//{
+//    Console.WriteLine(item);
 
-}
+//}
 
-Console.WriteLine("\n");
+//Console.WriteLine("\n");
 
-//newMenu.runMenu();
+newMenu.runMenu();
